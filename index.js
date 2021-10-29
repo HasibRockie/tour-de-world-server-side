49cb4e8dde547d7395c6d528adfe2c8886bef03f
Md Hasibul Islam <hasiee8004@gmail.com>
2021-10-30 00:13:56 +0600

    updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const ObjectId = require("mongodb").ObjectId;
 app.use(cors());
 app.use(express.json());
 
@@ -10,36 +11,77 @@ app.get("/", (req, res) => {
   res.send("Tour de world is running");
 });
 
-// PerosonalDiary
-// 6duW9uZ7gbAmMiEf
+// TourDeWorld
+// 7IWEHldps84qGnZH
 
-// const uri =
-//   "mongodb+srv://PerosonalDiary:6duW9uZ7gbAmMiEf@cluster0.6bqvy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+const uri =
+  "mongodb+srv://TourDeWorld:7IWEHldps84qGnZH@cluster0.xllaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function run() {
   try {
-    // await client.connect();
-    // const database = client.db("notes");
-    // const NoteCollections = database.collection("NoteCollections");
+    await client.connect();
+    const database = client.db("TourDeWorld");
+    const servicesCollections = database.collection("services");
+    const userCollections = database.collection("users");
 
-    // post method  
-    // app.post("/notes", async (req, res) => {
-    //   const entity = req.body;
-    //   const result = await NoteCollections.insertOne(entity);
-    //   res.json(result)
-    // });
+    // post method
+    app.post("/services", async (req, res) => {
+      const entity = req.body;
+      const result = await servicesCollections.insertOne(entity);
+      res.json(result);
+    });
 
-    // get method 
-    // app.get('/notes', async(req,res)=> {
-    //     const cursor =  NoteCollections.find({});
-    //     const result = await cursor.toArray()
-    //     res.send(result)
-    // })
+    // post method for users
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+          $set: {
+              user: updatedUser.user,
+              cart: updatedUser.cart,
+              orders: updatedUser.orders
+          },
+      };
+      const result = await userCollections.updateOne(filter, updateDoc, options)
+      console.log('updating', id)
+      res.json(result)
+  })
 
+    // get method
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollections.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get method for users
+    app.get("/users", async (req, res) => {
+      const cursor = userCollections.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get method for single user
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollections.findOne(query)
+      res.send(result);
+    });
+
+    // get method for single services
+    app.get("/services/:service", async (req, res) => {
+      const service = req.params.service;
+      const query = { _id: ObjectId(service) };
+      const result = await servicesCollections.findOne(query)
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
